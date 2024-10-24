390dbc511768b9d6b708dc138655ddce30ce3013
Carloscv200313 <carlosscv200313@gmail.com>
2024-10-23 22:05:03 -0500

    Servimach V:7.0 con backend 2.0

--- paginas/pagina-01/paginas-05.tsx
+++ paginas/pagina-01/paginas-05.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
 interface Pagina {
     scroll: () => void;
 }
-export default function Pagina01({ scroll }: Pagina){
+interface Usuarios {
+    id_usuario: string
+    nombre: string
+    dni: string
+    email:  string
+    telefono:  string
+    direccion:string
+}
+async function Usuario(): Promise<Usuarios[]> {
+    const result = await fetch("http://localhost:3000/api/usuarios");
+    if (!result.ok) {
+        throw new Error('Error al obtener los usuarios');
+    }
+    return result.json();
+}
+export default function Pagina01({ scroll }: Pagina) {
+    const [resultados, setresultados] = useState<Usuarios[]>([])
+    useEffect(() => {
+        Usuario()
+            .then(data => {
+                setresultados(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
     return (
         <div className="min-h-[80vh] flex flex-col items-center justify-center w-full p-10 bg-gradient-to-b from-[#62f1de] to-blue-400 gap-10">
             <motion.div
                 className='text-center text-4xl md:text-8xl font-extrabold font-sans  text-white '
-                initial={{ opacity: 0, x:-100 }} // Estado inicial
+                initial={{ opacity: 0, x: -100 }} // Estado inicial
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1 }} // Duración de la animación
             >
@@ -31,6 +56,11 @@ export default function Pagina01({ scroll }: Pagina){
             >
                 Regístrate Ya!!
             </motion.button>
+            <div>
+                {resultados.map((resultado) => 
+                    <h1 key={resultado.nombre}>{resultado.nombre} </h1>)
+                }
+            </div>
         </div>
     )
 }
